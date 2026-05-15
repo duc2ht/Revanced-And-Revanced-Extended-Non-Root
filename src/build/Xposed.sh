ae7c078d3e857acad3d0b310bea01d9a4403dcc0
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-05-16 01:42:34 +0700

    typo

--- src/build/Xposed.sh
+++ src/build/Xposed.sh
@@ -21,7 +21,7 @@ LSPatch_dl(){
 	lspatch "facebook-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh"
 	# Patch Messenger:
 	get_apk "com.facebook.orca" "messenger-arm64-v8a" "apk" "arm64-v8a" "nodpi" "Android 9.0+"
-	lspatch "messenger-arm64-v8a" ""NexAlloy-nonroot-release*.apk" "gnadgnaoh"
+	lspatch "messenger-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh"
 	# Patch Instagram:
 	get_apk "com.instagram.android" "instagram-arm64-v8a" "bundle" "arm64-v8a" "120-640dpi"  "Android 9.0+"
 	lspatch "instagram-arm64-v8a" "NexAlloy-nonroot-release*.apk" "gnadgnaoh"
